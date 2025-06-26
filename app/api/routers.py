cc2ed2f2a4b047358908f0eddad7deb1579a7fdc
mxcoras <2382413024@qq.com>
2025-06-26 20:34:04 +0800

    chore: 删除过时的配置项

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import chat, sing
+from app.api.endpoints import chat, sing, tts
 
 router = APIRouter()
 router.include_router(sing.router)
 router.include_router(chat.router)
+router.include_router(tts.router)

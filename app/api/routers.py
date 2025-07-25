1bed368f761b404fabf14a29e84a174c1a74bdc2
TogetsuDo <togetsudo@outlook.com>
2025-07-25 19:13:34 +0800

    feat: 添加网易云音乐登录功能及相关接口 (#5)

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import chat, sing, tts
+from app.api.endpoints import chat, ncm_login, sing, tts
 
 router = APIRouter()
 router.include_router(sing.router)
 router.include_router(chat.router)
 router.include_router(tts.router)
+router.include_router(ncm_login.router)
